e3c3200b1c7fdf2e64ebe46f2e757737341a8734
hammad1985 <hammad.yaqoob1985@yahoo.co.uk>
2020-01-02 23:50:10 +0000

    added passing down of onclick handler

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -8,7 +8,14 @@ class QuizQuestion extends Component {
         
     }
 
+    handleClick(buttonText) {
+      if(buttonText === this.props.quiz_question.answer) {
+        this.props.showNextQuestionHandler();
+      }
+    };
+    
     render() {
+       
         return (
             <main>
             <section>
@@ -16,7 +23,9 @@ class QuizQuestion extends Component {
             </section>
             <section className="buttons">
               <ul>
-                <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]}/>
+                {this.props.quiz_question.answer_options.map((answer_option, index) => { 
+                  return <QuizQuestionButton key={index} clickHandler={this.handleClick.bind(this)} button_text={answer_option}/>
+                })}
               </ul>
             </section>
           </main>
